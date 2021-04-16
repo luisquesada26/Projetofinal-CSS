25538a0ade1d39d6f18ec9202d9b06d5082983e6
luisquesada26 <80924241+luisquesada26@users.noreply.github.com>
2021-04-16 19:39:25 -0300

    Adicionando index

---
+++ menu.js
@@ -0,0 +1,8 @@
+let show = true;
+
+const menuSection = document.querySelector(".menu-section")
+
+menuSection.addEventListener("click",()=>{
+    menuSection.classList.toggle("on", show)
+    show = !show
+})
